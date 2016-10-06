2574d6f126fc306319eb56e7e89480de779c3bcf
sameer-wanwey <sameer.wanwey@gmail.com>
2016-10-06 00:35:53 -0600

    Model

---
+++ PeopleSearchAtHealthCatalyst.Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PeopleSearchAtHealthCatalyst.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Interests { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
